328b4349df34fe4197d11b71d59d2f7afcab0449
LouisGaboury <louis.gaboury.4@gmail.com>
2021-10-05 18:04:40 +0200

    Adding hover effect on button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import react from "react";
 
 const Button = () => {
-    return <div className="p-4 max-w-xs w-40 bg-blue-400 rounded-xl shadow-md">
-        <p className="text-gray-800 font-bold text-center text-lg">Dissoudre</p>
+    return <div className="p-4 max-w-xs w-40 bg-blue-400 rounded-xl shadow-md hover:bg-blue-700">
+        <p className="text-blue-200 font-bold text-center text-lg">Dissoudre</p>
     </div>
 }
 
