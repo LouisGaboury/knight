aeee7f5728ccf8b44bd7f044ec8bb48b88cfec1b
LouisGaboury <louis.gaboury.4@gmail.com>
2021-10-05 17:57:57 +0200

    Trying to create a base button with Tailwind

---
+++ src/components/Button.js
@@ -0,0 +1,9 @@
+import react from "react";
+
+const Button = () => {
+    return <div className="p-4 max-w-xs w-40 bg-blue-400 rounded-xl shadow-md">
+        <p className="text-gray-800 font-bold text-center text-lg">Dissoudre</p>
+    </div>
+}
+
+export default Button;
