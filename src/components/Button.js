a29f23443c2849eea93580b234b9e54f15d554d2
LouisGaboury <louis.gaboury.4@gmail.com>
2021-10-05 18:41:57 +0200

    Adding smooth transition on Button hover

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,9 @@
 import react from "react";
 
 const Button = () => {
-    return <div className="p-4 max-w-xs w-40 bg-blue-400 rounded-xl shadow-md hover:bg-blue-700">
+    return <button className="p-4 max-w-xs w-40 bg-blue-400 rounded-xl shadow-md hover:bg-blue-700 transform transition duration-300 ease-in-out">
         <p className="text-blue-200 font-bold text-center text-lg">Dissoudre</p>
-    </div>
+    </button>
 }
 
 export default Button;
