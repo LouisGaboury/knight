2755452fcc9402ca9e8832fc7eb2bbb0edd5e778
LouisGaboury <louis.gaboury.4@gmail.com>
2021-10-07 01:52:51 +0200

    Adding Login page without logic

---
+++ src/components/ConnexionForm.js
@@ -0,0 +1,18 @@
+const ConnexionForm = (props) => {
+  return (
+    <form className="mb-36">
+      <fieldset>
+        <div className="flex flex-col mx-4 mb-4">
+          <label for="login">login</label>
+          <input type="text" name="login" id="login"></input>
+        </div>
+        <div className="flex flex-col mx-4 mb-4">
+          <label for="password">mot de passe</label>
+          <input type="text" name="password" id="password"></input>
+        </div>
+      </fieldset>
+    </form>
+  );
+};
+
+export default ConnexionForm;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,8 +3,17 @@ import Button from "../components/Button";
 import ConnexionForm from "../components/ConnexionForm";
 
 const Login = (props) => {
+  // TODO - add state for connected or not connected
+  // TODO - make previous state bubble-up
+  // TODO - link with Supabase for testing API connexion
+
   return (
-    <div className="flex flex-col justify-center rounded-xs mx-auto mt-12 shadow-md w-4/6 h-3/4">
+    <div className="flex flex-col justify-center rounded-xs mx-auto mt-12 shadow-md w-4/6 max-w-lg h-3/4">
+      <img
+        src="/logoK32.png"
+        alt="logo de Knight"
+        className="max-w-md mx-auto mb-28"
+      />
       <ConnexionForm />
       <Button textButton="Connexion" />
     </div>
