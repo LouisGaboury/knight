01b808f7b30f78adc657a91afabeb50f8005f73c
LouisGaboury <louis.gaboury.4@gmail.com>
2021-10-06 21:35:41 +0200

    Adding linter to project

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,11 @@
 const Button = (props) => {
-    return <button className="p-4 max-w-xs max-h-16 w-40 bg-blue-400 rounded-xl shadow-md hover:bg-blue-700 transform transition duration-300 ease-in-out">
-        <p className="text-blue-200 font-bold text-center text-lg">{props.textButton}</p>
+  return (
+    <button className="p-4 max-w-xs max-h-16 w-40 bg-blue-400 rounded-xl shadow-md hover:bg-blue-700 transform transition duration-300 ease-in-out">
+      <p className="text-blue-200 font-bold text-center text-lg">
+        {props.textButton}
+      </p>
     </button>
-}
+  );
+};
 
 export default Button;
--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,12 +1,14 @@
-import react from 'react';
-import Button from '../components/Button'
+import react from "react";
+import Button from "../components/Button";
+import ConnexionForm from "../components/ConnexionForm";
 
 const Login = (props) => {
-    return (
-        <div className="flex justify-center rounded-xs mx-auto mt-12 shadow-md w-4/6 h-3/4">
-            <Button textButton="Connexion" />
-        </div>
-    )
-}
+  return (
+    <div className="flex justify-center rounded-xs mx-auto mt-12 shadow-md w-4/6 h-3/4">
+      <ConnexionForm />
+      <Button textButton="Connexion" />
+    </div>
+  );
+};
 
 export default Login;
