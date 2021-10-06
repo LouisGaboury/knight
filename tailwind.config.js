fb5b2b1ad9b04e0f68b8d4732a782f59ad4b078c
LouisGaboury <louis.gaboury.4@gmail.com>
2021-10-06 23:00:42 +0200

    Importing forms utilitaries for Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
-const colors = require("tailwindcss/colors")
+const colors = require("tailwindcss/colors");
 
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
     color: {
       transparent: "transparent",
       blue: colors.sky,
-      gray: colors.blueGray
-    }
+      gray: colors.blueGray,
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require("@tailwindcss/forms")],
+};
