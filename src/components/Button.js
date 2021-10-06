2ee8681f37be1c77864683d44e13b6db29c75182
LouisGaboury <louis.gaboury.4@gmail.com>
2021-10-06 22:18:33 +0200

    Adding automatic margin upon Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 const Button = (props) => {
   return (
-    <button className="p-4 max-w-xs max-h-16 w-40 bg-blue-400 rounded-xl shadow-md hover:bg-blue-700 transform transition duration-300 ease-in-out">
+    <button className="p-4 mx-auto max-w-xs max-h-16 w-40 bg-blue-400 rounded-xl shadow-md hover:bg-blue-700 transform transition duration-300 ease-in-out">
       <p className="text-blue-200 font-bold text-center text-lg">
         {props.textButton}
       </p>
