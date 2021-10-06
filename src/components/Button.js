ee5b2d4f6baf279389bd726130807445da90636a
LouisGaboury <louis.gaboury.4@gmail.com>
2021-10-06 19:12:25 +0200

    Adding a max-height to Button component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,5 @@
 const Button = (props) => {
-    return <button className="p-4 max-w-xs w-40 bg-blue-400 rounded-xl shadow-md hover:bg-blue-700 transform transition duration-300 ease-in-out">
+    return <button className="p-4 max-w-xs max-h-16 w-40 bg-blue-400 rounded-xl shadow-md hover:bg-blue-700 transform transition duration-300 ease-in-out">
         <p className="text-blue-200 font-bold text-center text-lg">{props.textButton}</p>
     </button>
 }
