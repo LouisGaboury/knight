80c21a56e345b2c57c4438ea107ecce64965efba
LouisGaboury <louis.gaboury.4@gmail.com>
2021-10-13 23:57:16 +0200

    Adding a ring arround Button when focus

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,6 @@
 const Button = (props) => {
   return (
-    <button className="p-4 mx-auto max-w-xs max-h-16 w-40 bg-blue-400 rounded-xl shadow-md hover:bg-blue-700 transform transition duration-300 ease-in-out">
+    <button className="p-4 mx-auto max-w-xs max-h-16 w-40 bg-blue-400 rounded-xl shadow-md hover:bg-blue-700 transform transition duration-300 ease-in-out focus:ring-4">
       <p className="text-blue-200 font-bold text-center text-lg">
         {props.textButton}
       </p>
