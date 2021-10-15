d335ca4bf77b2c46fe57b977c4776cd35054413d
LouisGaboury <louis.gaboury.4@gmail.com>
2021-10-15 11:35:04 +0200

    Adding comments + refactoring + functionnal form

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,5 +1,4 @@
 import { Fragment, useState } from "react";
-import ConnexionForm from "./ConnexionForm";
 import Button from "./Button";
 import { supabase } from "../supabaseClient";
 
@@ -9,12 +8,20 @@ const SignIn = (props) => {
     password: "",
   });
 
+  /**
+   * @async
+   * Permet de connecter un user inscrit dans la BDD
+   * @todo sauvegarder ce que renvoie la connexion dans un state
+   * @todo Chiffrer les password
+   */
   const handleLogin = async () => {
     try {
+      // Se connecte à Supabase pour chercher le user
       const { user, session, error } = await supabase.auth.signIn({
         email: userCredentials.email,
         password: userCredentials.password,
       });
+      // Si erreur => soulève l'erreur
       if (error) throw error;
       console.log(user);
       console.log(session);
@@ -25,10 +32,45 @@ const SignIn = (props) => {
 
   return (
     <Fragment>
-      <ConnexionForm />
+      <form className="mb-24">
+        <fieldset>
+          <div className="flex flex-col mx-4 mb-4">
+            <label>login</label>
+            <input
+              type="text"
+              placeholder="votre email"
+              value={userCredentials.email}
+              onChange={(event) => {
+                setCredentials((prevState) => ({
+                  // Charge le state précédent
+                  ...prevState,
+                  // Ne change que la valeur qui nous intéresse
+                  email: event.target.value,
+                }));
+              }}
+            ></input>
+          </div>
+          <div className="flex flex-col mx-4 mb-4">
+            <label>mot de passe</label>
+            <input
+              type="password"
+              value={userCredentials.password}
+              onChange={(event) => {
+                setCredentials((prevState) => ({
+                  // Charge le state précédent
+                  ...prevState,
+                  // Ne change que la valeur qui nous intéresse
+                  password: event.target.value,
+                }));
+              }}
+            ></input>
+          </div>
+        </fieldset>
+      </form>
       <Button
         textButton="Connexion"
         onClick={(event) => {
+          // Utilisation de preventDefault() pour empêcher le rafraichissement de la page
           event.preventDefault();
           handleLogin();
         }}
