913d2c06f9b2b964ca711d6a19c8b4fe0639cbd3
LouisGaboury <louis.gaboury.4@gmail.com>
2021-10-20 17:30:09 +0200

    Adding comments

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,12 @@ import Header from "../components/Header";
 
 const Home = () => {
   const [user, setUser] = useState({});
-  // Loading -> empêche d'
+  // Loading -> empêche d'afficher tant que le user n'est pas défini
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     setUser(supabase.auth.user());
+    // Une fois qu'on a le user -> on provoque l'affichage général
     setLoading(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
