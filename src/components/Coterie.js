0473a5cd7be038875cdfa1d18135b30d09c753c3
LouisGaboury <louis.gaboury.4@gmail.com>
2021-10-26 17:39:10 +0200

    Making coteries be displayed one at the time

--- src/components/Coterie.js
+++ src/components/Coterie.js
@@ -9,6 +9,30 @@ const Coterie = ({ getCoteries }) => {
     setCoteries(getCoteries());
   }, [getCoteries]);
 
+  // Allow to control witch coterie is shown
+  const changeFocus = (event) => {
+    if (event.target.id === "forth") {
+      // If we are at the end of the array
+      if (focus === coteries.length - 1) {
+        // go back to zero
+        setFocus(0);
+      } else {
+        // else advance
+        setFocus(focus + 1);
+      }
+    } else {
+      // If we are at the beginning of the array
+      if (focus === 0) {
+        // go to the end of the array
+        setFocus(coteries.length - 1);
+      } else {
+        // move back from 1 unit
+        setFocus(focus - 1);
+      }
+    }
+  };
+
+  // Just display nothing if component doesn't set yet
   const displayLoading = () => {
     return <Fragment />;
   };
@@ -16,11 +40,57 @@ const Coterie = ({ getCoteries }) => {
   const displayLoaded = () => {
     return (
       <section>
-        <h5>Coterie n°{coteries[focus].id}</h5>
+        <h5 className="mb-6 mt-2 text-center text-2xl">
+          Coterie n°{coteries[focus].id}
+        </h5>
+        <div className="flex justify-between">
+          <button
+            onClick={changeFocus}
+            id="back"
+            className="p-4 max-w-xs max-h-16 bg-blue-400 rounded-xl shadow-md relative -left-8 hover:bg-blue-700 transform transition duration-300 ease-in-out focus:ring-4"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+          <button
+            onClick={changeFocus}
+            id="forth"
+            className="p-4 max-w-xs max-h-16 bg-blue-400 rounded-xl shadow-md relative -right-8 hover:bg-blue-700 transform transition duration-300 ease-in-out focus:ring-4"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              />
+              <path
+                fillRule="evenodd"
+                d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
       </section>
     );
   };
 
+  // If component set : display. Else : display nothing
   return coteries.length > 0 ? displayLoaded() : displayLoading();
 };
 
