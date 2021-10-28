6eea0431bee99fdb995b1225509571a0048045db
LouisGaboury <louis.gaboury.4@gmail.com>
2021-10-28 15:45:20 +0200

    Refactoring fetch + conditionnal display

--- src/components/Coterie.js
+++ src/components/Coterie.js
@@ -1,16 +1,18 @@
 import { Fragment, useEffect, useState } from "react";
+import { findCoteries } from "../services/supabase/supabase";
 import { CircularProgressbar } from "react-circular-progressbar";
 import "react-circular-progressbar/dist/styles.css";
 import ActionButton from "./ActionButton";
 
-const Coterie = ({ getCoteries }) => {
-  const [coteries, setCoteries] = useState([]);
+const Coterie = (props) => {
+  const [coteries, setCoteries] = useState(null);
   const [focus, setFocus] = useState(0);
 
-  // Se déclenche dès que getCoteries est updated
   useEffect(() => {
-    setCoteries(getCoteries());
-  }, [getCoteries]);
+    if (props.idSection) {
+      findCoteries(props.idSection).then((res) => setCoteries(res));
+    }
+  }, [props]);
 
   // Allow to control witch coterie is shown
   const changeFocus = (event) => {
@@ -35,122 +37,117 @@ const Coterie = ({ getCoteries }) => {
     }
   };
 
-  // Just display nothing if component doesn't set yet
-  const displayLoading = () => {
-    return <Fragment />;
-  };
-
-  const displayLoaded = () => {
-    return (
-      <section>
-        <h5 className="mb-6 mt-4 text-center text-3xl">
-          Coterie n°{coteries[focus].id}
-        </h5>
-        {/* boutons de switch sur le côté */}
-        <div className="flex justify-between">
-          <button
-            onClick={changeFocus}
-            id="back"
-            className="p-4 max-w-xs max-h-16 bg-blue-400 rounded-xl shadow-md relative -left-8 hover:bg-blue-700 transform transition duration-300 ease-in-out focus:ring-4"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-          <button
-            onClick={changeFocus}
-            id="forth"
-            className="p-4 max-w-xs max-h-16 bg-blue-400 rounded-xl shadow-md relative -right-8 hover:bg-blue-700 transform transition duration-300 ease-in-out focus:ring-4"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              viewBox="0 0 20 20"
-              fill="currentColor"
-            >
-              <path
-                fillRule="evenodd"
-                d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
-                clipRule="evenodd"
-              />
-              <path
-                fillRule="evenodd"
-                d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </button>
-        </div>
-        {/* indicateurs circulaires (santé, pex, espoir) */}
-        <div className="relative mb-10 -top-10 flex flex-row justify-around mx-10">
-          <div className="w-40 h-40">
-            <p className="mb-4 text-center text-lg font-bold">Santé</p>
-            <CircularProgressbar
-              value={coteries[focus].health}
-              text={`${coteries[focus].health}%`}
-              styles={{
-                path: {
-                  stroke: "#d1280f",
-                },
-                text: {
-                  fill: "#d1280f",
-                },
-              }}
-            />
-          </div>
-          <div className="w-40 h-40">
-            <p className="mb-4 text-center text-lg font-bold">Espoir</p>
-            <CircularProgressbar
-              value={coteries[focus].hope}
-              text={`${coteries[focus].hope}%`}
-            />
-          </div>
-          <div className="w-40 h-40">
-            <p className="mb-4 text-center text-lg font-bold">XP</p>
-            <CircularProgressbar
-              value={coteries[focus].xp}
-              text={`${coteries[focus].xp}%`}
-            />
-          </div>
-        </div>
-        {/* historique et boutons d'action */}
-        <div className="flex">
-          {/* historique */}
-          <div className="w-1/2">
-            <ul>
-              <li>Sénéchal :</li>
-              <li>Rang : {coteries[focus].rank}</li>
-              <li>Mission actuelle :</li>
-              <li>Missions totales :</li>
-            </ul>
-          </div>
-          {/* boutons d'action */}
-          <div className="flex flex-col justify-around w-1/2">
-            <div className="flex justify-around mb-4">
-              <ActionButton textButton={"Se reposer"} />
-              <ActionButton textButton={"Entrainer"} />
-            </div>
-            <div className="flex justify-around">
-              <ActionButton textButton={"Rappeler"} />
-              <ActionButton textButton={"Dissoudre"} />
-            </div>
-          </div>
-        </div>
-      </section>
-    );
-  };
-
   // If component set : display. Else : display nothing
-  return coteries.length > 0 ? displayLoaded() : displayLoading();
+  return (
+    <section>
+      {coteries && (
+        <Fragment>
+          <h5 className="mb-6 mt-4 text-center text-3xl">
+            Coterie n°{coteries[focus].id}
+          </h5>
+          {/* boutons de switch sur le côté */}
+          <div className="flex justify-between">
+            <button
+              onClick={changeFocus}
+              id="back"
+              className="p-4 max-w-xs max-h-16 bg-blue-400 rounded-xl shadow-md relative -left-8 hover:bg-blue-700 transform transition duration-300 ease-in-out focus:ring-4"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M15.707 15.707a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 010 1.414zm-6 0a1 1 0 01-1.414 0l-5-5a1 1 0 010-1.414l5-5a1 1 0 011.414 1.414L5.414 10l4.293 4.293a1 1 0 010 1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+            <button
+              onClick={changeFocus}
+              id="forth"
+              className="p-4 max-w-xs max-h-16 bg-blue-400 rounded-xl shadow-md relative -right-8 hover:bg-blue-700 transform transition duration-300 ease-in-out focus:ring-4"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.293 15.707a1 1 0 010-1.414L14.586 10l-4.293-4.293a1 1 0 111.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+                  clipRule="evenodd"
+                />
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 15.707a1 1 0 010-1.414L8.586 10 4.293 5.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+          {/* indicateurs circulaires (santé, pex, espoir) */}
+          <div className="relative mb-10 -top-10 flex flex-row justify-around mx-10">
+            <div className="w-40 h-40">
+              <p className="mb-4 text-center text-lg font-bold">Santé</p>
+              <CircularProgressbar
+                value={coteries[focus].health}
+                text={`${coteries[focus].health}%`}
+                styles={{
+                  path: {
+                    stroke: "#d1280f",
+                  },
+                  text: {
+                    fill: "#d1280f",
+                  },
+                }}
+              />
+            </div>
+            <div className="w-40 h-40">
+              <p className="mb-4 text-center text-lg font-bold">Espoir</p>
+              <CircularProgressbar
+                value={coteries[focus].hope}
+                text={`${coteries[focus].hope}%`}
+              />
+            </div>
+            <div className="w-40 h-40">
+              <p className="mb-4 text-center text-lg font-bold">XP</p>
+              <CircularProgressbar
+                value={coteries[focus].xp}
+                text={`${coteries[focus].xp}%`}
+              />
+            </div>
+          </div>
+          {/* historique et boutons d'action */}
+          <div className="flex">
+            {/* historique */}
+            <div className="w-1/2">
+              <ul>
+                <li>Sénéchal :</li>
+                <li>Rang : {coteries[focus].rank}</li>
+                <li>Mission actuelle :</li>
+                <li>Missions totales :</li>
+              </ul>
+            </div>
+            {/* boutons d'action */}
+            <div className="flex flex-col justify-around w-1/2">
+              <div className="flex justify-around mb-4">
+                <ActionButton textButton={"Se reposer"} />
+                <ActionButton textButton={"Entrainer"} />
+              </div>
+              <div className="flex justify-around">
+                <ActionButton textButton={"Rappeler"} />
+                <ActionButton textButton={"Dissoudre"} />
+              </div>
+            </div>
+          </div>
+        </Fragment>
+      )}
+    </section>
+  );
 };
 
 export default Coterie;
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,30 +1,14 @@
-import { useEffect, useState, useCallback } from "react";
-import { findSection, findCoteries } from "../services/supabase/supabase";
+import { useEffect, useState } from "react";
+import { findSection } from "../services/supabase/supabase";
 import { Section as SectionClass } from "../services/supabase/classes";
 import Coterie from "./Coterie";
 
 const Section = (props) => {
   const [section, setSection] = useState(new SectionClass());
-  const [coteries, setCoteries] = useState([]);
 
-  // Se déclenche une seule fois au chargement du composant
-  // Architecture dégueulasse mais forcée par React
   useEffect(() => {
-    async function setRessources() {
-      const newSection = await findSection(props.userID);
-      const newCoteries = await findCoteries(newSection.id);
-      // Les deux fonctions ci dessous sont asynchrones
-      setSection(newSection);
-      setCoteries(newCoteries);
-    }
-    setRessources();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    findSection(props.userID).then((res) => setSection(res));
   }, []);
-
-  // Se déclenche dès que coteries est updated
-  const getCoteries = useCallback(() => {
-    return coteries;
-  }, [coteries]);
 
   return (
     <div className="w-4/5 shadow-md bg-gray-100 mx-auto mt-8">
@@ -34,7 +18,7 @@ const Section = (props) => {
         possédés, etc. Ca sera fixe
       </p>
       <hr className="border-2 border-solid" />
-      <Coterie getCoteries={getCoteries} />
+      <Coterie idSection={section.id} />
     </div>
   );
 };
