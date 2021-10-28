2fc032b003214b22b3da66fdbf7675e0c5bcb48a
LouisGaboury <louis.gaboury.4@gmail.com>
2021-10-28 15:38:22 +0200

    Refactoring conditionnal display

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,31 +5,23 @@ import Header from "../components/Header";
 import Section from "../components/Section";
 
 const Home = () => {
-  const [user, setUser] = useState({});
-  // Loading -> empêche d'afficher tant que le user n'est pas défini
-  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(null);
 
   useEffect(() => {
     setUser(supabase.auth.user());
-    // Une fois qu'on a le user -> on provoque l'affichage général
-    setLoading(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const displayLoading = () => {
-    return <Fragment />;
-  };
-
-  const displayLoaded = () => {
-    return (
-      <Fragment>
-        <Header userID={user.id} />
-        <Section userID={user.id} />
-      </Fragment>
-    );
-  };
-
-  return !loading ? displayLoaded() : displayLoading();
+  return (
+    <Fragment>
+      {user && (
+        <Fragment>
+          <Header userID={user.id} />
+          <Section userID={user.id} />
+        </Fragment>
+      )}
+    </Fragment>
+  );
 };
 
 export default Home;
