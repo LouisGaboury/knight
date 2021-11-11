9d7220adf8d1f7207cd854ea32a934eda1d2e2de
LouisGaboury <louis.gaboury.4@gmail.com>
2021-11-11 12:59:22 +0100

    Basic layers control system

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,22 +1,36 @@
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  LayersControl,
+  Circle,
+} from "react-leaflet";
 
 const Map = () => {
   return (
     <MapContainer
       center={[51.505, -0.09]}
-      zoom={13}
-      scrollWheelZoom={false}
+      zoom={4}
+      maxZoom={8}
+      minZoom={3}
+      scrollWheelZoom={true}
       className={"h-full w-full"}
     >
-      <TileLayer
-        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <LayersControl position="topright">
+        <TileLayer
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <LayersControl.BaseLayer checked name="Arches">
+          <Marker position={[51.49939, -0.124754]}>
+            <Popup>QG du Knight</Popup>
+          </Marker>
+        </LayersControl.BaseLayer>
+        <LayersControl.BaseLayer name="Tâches d'Anathème">
+          <Circle center={[48.8534, 2.3488]} radius={20000} />
+        </LayersControl.BaseLayer>
+      </LayersControl>
     </MapContainer>
   );
 };
