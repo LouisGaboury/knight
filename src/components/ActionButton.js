ce056de61a5512ec9275898e09a2b0041b2adcf2
LouisGaboury <louis.gaboury.4@gmail.com>
2021-11-15 18:24:17 +0100

    Adding no-margin rules for button's text

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -4,7 +4,7 @@ const ActionButton = (props) => {
       onClick={props.onClick}
       className="p-4 mx-auto max-w-xs max-h-16 w-40 bg-blue-400 rounded-xl shadow-md hover:bg-blue-700 transform transition duration-300 ease-in-out focus:ring-4"
     >
-      <p className="text-blue-200 font-bold text-center text-lg">
+      <p className="text-blue-200 font-bold text-center text-lg my-0">
         {props.textButton}
       </p>
     </button>
