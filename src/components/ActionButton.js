902cd1b97e53c5f4457aa3ea8ea657f31e8b1408
LouisGaboury <louis.gaboury.4@gmail.com>
2022-01-07 17:38:59 +0100

    Adding disabling style

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -2,7 +2,10 @@ const ActionButton = (props) => {
   return (
     <button
       onClick={props.onClick}
-      className="p-4 mx-auto max-w-xs max-h-16 w-40 bg-blue-400 rounded-xl shadow-md hover:bg-blue-700 transform transition duration-300 ease-in-out focus:ring-4"
+      className={`p-4 mx-auto max-w-xs max-h-16 w-40 rounded-xl shadow-md hover:bg-blue-700 transform transition duration-300 ease-in-out focus:ring-4 ${
+        props.disabled ? "bg-gray-800" : "bg-blue-400"
+      }`}
+      disabled={props.disabled}
     >
       <p className="text-blue-200 font-bold text-center text-lg my-0">
         {props.textButton}
