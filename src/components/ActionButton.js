3dfee4a265d66be9e5f875aee4538d63edbcd55e
LouisGaboury <louis.gaboury.4@gmail.com>
2022-01-07 18:09:47 +0100

    Better visualizing disabled button

--- src/components/ActionButton.js
+++ src/components/ActionButton.js
@@ -2,8 +2,10 @@ const ActionButton = (props) => {
   return (
     <button
       onClick={props.onClick}
-      className={`p-4 mx-auto max-w-xs max-h-16 w-40 rounded-xl shadow-md hover:bg-blue-700 transform transition duration-300 ease-in-out focus:ring-4 ${
-        props.disabled ? "bg-gray-800" : "bg-blue-400"
+      className={`p-4 mx-auto max-w-xs max-h-16 w-40 rounded-xl shadow-md ${
+        props.disabled
+          ? "bg-gray-800"
+          : "bg-blue-400 focus:ring-4 hover:bg-blue-700 transform transition duration-300 ease-in-out"
       }`}
       disabled={props.disabled}
     >
