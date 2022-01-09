92590e461331c54533951fa69245a2f5a92499e9
LouisGaboury <louis.gaboury.4@gmail.com>
2022-01-09 22:20:48 +0100

    Fixing margin

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -6,7 +6,7 @@ const NotFound = () => {
     <Fragment>
       <Header />
       <h2 className="text-7xl mt-16 ml-16">404 not found</h2>
-      <p className="text-3xl mt-8 ml-16">
+      <p className="text-3xl mt-8 mx-16">
         Attention, Chevalier ! Vous vous aventurez dans les Abysses. Nous vous
         conseillons vivement de retourner en zone sûre !
       </p>
