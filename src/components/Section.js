d8a26937ef87eb33f9624816c404b88d3a8b1946
LouisGaboury <louis.gaboury.4@gmail.com>
2022-07-18 11:20:00 +0200

    Refactoring component Section

--- src/components/Section.js
+++ src/components/Section.js
@@ -1,54 +1,31 @@
-import { useEffect, useState, Fragment } from "react";
-import {
-  getSectionByUser,
-  confirmActivity,
-} from "../services/supabase/supabase";
-import { supabase } from "../supabaseClient";
+import { Fragment } from "react";
+import { confirmActivity } from "../services/supabase/supabase";
 import CoteriesSlider from "./coterie/CoteriesSlider";
-import MissionsSlider from "./missions/MissionsSlider";
-import Reports from "./reports/Reports";
 import ActionButton from "./utility/ActionButton";
 
-const Section = () => {
-  const [section, setSection] = useState(null);
-
-  useEffect(() => {
-    getSectionByUser(supabase.auth.user().id).then((res) => setSection(res));
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
+const Section = ({ section }) => {
   const handleConfirmActivity = async () => {
     const result = await confirmActivity(section.id);
     if (result) alert("Votre demande a bien été envoyée");
   };
 
   return (
-    <Fragment>
-      <div className="w-4/5 shadow-md bg-gray-100 mx-auto mt-8">
-        <div className="p-4">
-          <h3 className="text-6xl pt-4 mb-8 text-center">{section?.name}</h3>
-          <p>
-            Ici on affichera les diverses infos relatives à la section : RG,
-            objets possédés, etc. Ca sera fixe
-          </p>
-          <ActionButton
-            textButton="Passer le jour"
-            onClick={handleConfirmActivity}
-            mt="4"
-          />
-        </div>
-        <hr className="border-2 border-solid" />
-        <CoteriesSlider sectionID={section?.id} />
+    <div className="w-4/5 shadow-md bg-gray-100 mx-auto mt-8">
+      <div className="p-4">
+        <h3 className="text-6xl pt-4 mb-8 text-center">{section?.name}</h3>
+        <p>
+          Ici on affichera les diverses infos relatives à la section : RG,
+          objets possédés, etc. Ca sera fixe
+        </p>
+        <ActionButton
+          textButton="Passer le jour"
+          onClick={handleConfirmActivity}
+          mt="4"
+        />
       </div>
-      <div className="w-4/5 shadow-md bg-gray-100 mx-auto mt-8">
-        <h3 className="text-6xl pt-4 mb-8 text-center">Missions libres</h3>
-        {section && <MissionsSlider section={section} />}
-      </div>
-      <div className="w-4/5 shadow-md bg-gray-100 mx-auto my-8">
-        <h3 className="text-6xl pt-4 mb-8 text-center">Rapports</h3>
-        <Reports sectionID={section?.id} />
-      </div>
-    </Fragment>
+      <hr className="border-2 border-solid" />
+      <CoteriesSlider sectionID={section?.id} />
+    </div>
   );
 };
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,31 @@
-import { Fragment } from "react";
-
+import { Fragment, useState, useEffect } from "react";
+import MissionsSlider from "../components/missions/MissionsSlider";
+import Reports from "../components/reports/Reports";
 import Header from "../components/Header";
 import Section from "../components/Section";
+import { supabase } from "../supabaseClient";
+import { getSectionByUser } from "../services/supabase/supabase";
 
 const Home = () => {
+  const [section, setSection] = useState(null);
+
+  useEffect(() => {
+    getSectionByUser(supabase.auth.user().id).then((res) => setSection(res));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   return (
     <Fragment>
       <Header />
-      <Section />
+      <Section section={section} />
+      <div className="w-4/5 shadow-md bg-gray-100 mx-auto mt-8">
+        <h3 className="text-6xl pt-4 mb-8 text-center">Missions libres</h3>
+        {section && <MissionsSlider section={section} />}
+      </div>
+      <div className="w-4/5 shadow-md bg-gray-100 mx-auto my-8">
+        <h3 className="text-6xl pt-4 mb-8 text-center">Rapports</h3>
+        <Reports sectionID={section?.id} />
+      </div>
     </Fragment>
   );
 };
